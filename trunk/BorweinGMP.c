a00e868e4703ce58bf223573ac3d040dfba1da5b
faelsartori <faelsartori@d6401a51-91d3-217a-d17f-7f41952bd0b9>
2012-03-23 01:05:34 +0000

    Borwein com GMP correto

--- trunk/BorweinGMP.c
+++ trunk/BorweinGMP.c
@@ -35,14 +35,15 @@ int main(void)
 	x = 0;
 
 	// Iteracoes para calculo do numero Pi
-	while(i < 10000){
+	while(i < 1000){
 
 	//      yPos = (1 - pow(1-pow(yAnt,4),0.25)) / (1 + pow(1-pow(yAnt,4),0.25));
 
 		mpf_pow_ui(yAntAux, yAnt, 4);
 		mpf_ui_sub(yAntAux, 1, yAntAux);
-		mpf_pow_ui(yAntAux, yAntAux, 0.25);
 	
+		mpf_sqrt(yAntAux, yAntAux);
+		mpf_sqrt(yAntAux, yAntAux);
 		mpf_add_ui(yAntAux2, yAntAux, 1);
 		mpf_ui_sub(yAntAux, 1, yAntAux);
 
@@ -50,7 +51,7 @@ int main(void)
 
       	//	aPos = (aAnt*pow((1+yPos),4)) - pow(2,(2*i+3))*yPos*(1 + yPos + pow(yPos,2));
 
-		mpf_mul(aAntAux, yPos, yPos);
+		mpf_pow_ui(aAntAux, yPos, 2);
 		mpf_add(aAntAux, aAntAux, yPos);
 		mpf_add_ui(aAntAux, aAntAux, 1);
 		mpf_mul(aAntAux, yPos, aAntAux);
@@ -61,9 +62,7 @@ int main(void)
 		mpf_pow_ui(aAntAux2, aAntAux2, x);
 		mpf_mul(aAntAux, aAntAux, aAntAux2);
 
-		mpf_add_ui(yPos, yPos, 1);
-		mpf_set(aAntAux2, yPos);
-		mpf_sub_ui(yPos, yPos, 1);
+		mpf_add_ui(aAntAux2, yPos, 1);
 
 		mpf_pow_ui(aAntAux2, aAntAux2, 4);
 		mpf_mul(aAntAux2, aAnt, aAntAux2);
@@ -78,5 +77,5 @@ int main(void)
 
 	mpf_ui_div(pi, 1, aAnt);
 
-	gmp_printf("PI: %.1000Ff\n", pi);
+	gmp_printf("PI: %.10000Ff\n", pi);
 }
